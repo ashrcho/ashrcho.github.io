df78608fe806a60b69fd6d28242adc1776c197c2
Ashwin Chockkalingam <ashwin.chockkalingam@sjsu.edu>
2025-09-15 15:40:28 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ashrcho.github.io", // your domain
   vite: {
     plugins: [tailwindcss()],
   },
