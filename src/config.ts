fb8572ffbf0343650bbf533483c419268b0a54f1
Ashwin Chockkalingam <ashwin.chockkalingam@sjsu.edu>
2025-09-17 19:38:44 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
+// config.ts
 export const siteConfig = {
   name: "Ashwin Chockkalingam",
   title: "Business Analytics, Operations & Strategy",
@@ -11,6 +12,7 @@ export const siteConfig = {
   aboutMe:
     "I’m an Economics graduate with experience in business analysis, financial modeling, and data strategy. Through internships and projects, I’ve worked on models, dashboards, and making strategic recommendations. I bring a mix of analytical skills and business insight, making me well-suited to bridge data and strategy. I’m currently pursuing opportunities in business analytics, finance, and strategy/operations.",
   skills: ["Python", "SQL", "R", "Microsoft Excel", "Tableau", "SAP"],
+
   projects: [
     {
       name: "Sales KPI Dashboard",
@@ -22,16 +24,19 @@ export const siteConfig = {
     {
       name: "Predicting Customer Churn in Python",
       description:
-        "Machine learning model that predicts how likely customers are to churn. Achieving 93% accuracy",
+        "Machine learning model that predicts how likely customers are to churn, achieving 93% accuracy.",
       link: "https://fullstackextensions.com/?ref=devportfolio",
       skills: ["Python", "Machine Learning"],
+    },
+  ],
+
   experience: [
     {
       company: "Vetri Labs",
       title: "Business Analyst Intern",
       dateRange: "Feb 2024 - Jan 2025",
       bullets: [
-        "Built a financial model in Excel projecting 1-2 year growth, used by the CEO and leadership team to assess funding strategies and refine business goals.",
+        "Built a financial model in Excel projecting 1–2 year growth, used by the CEO and leadership team to assess funding strategies and refine business goals.",
         "Analyzed regional competitors’ market share, pricing, and customer segments to highlight differentiation opportunities and define the company’s unique value proposition.",
         "Initiated and led the development of the company’s SBA Growth Accelerator Fund application by compiling financial data and crafting strategic proposals that strengthened the company’s case for federal funding.",
       ],
@@ -46,18 +51,17 @@ export const siteConfig = {
       ],
     },
   ],
+
   education: [
     {
       school: "San Jose State University",
       degree: "Bachelor of Science in Economics",
       dateRange: "2023 - 2025",
-      ],
     },
     {
-      school: "University of California San Diego Extended Studied",
+      school: "University of California San Diego Extended Studies",
       degree: "Statistics for Data Analytics",
       dateRange: "2025",
-      ],
     },
   ],
-};
+} as const;
