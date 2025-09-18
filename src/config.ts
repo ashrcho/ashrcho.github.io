7bbdb0dca52b67cd6a643cbdf01b21b317428058
Ashwin Chockkalingam <ashwin.chockkalingam@sjsu.edu>
2025-09-17 20:04:00 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -11,6 +11,7 @@ export const siteConfig = {
   aboutMe:
     "I’m an Economics graduate with experience in business analysis, financial modeling, and data strategy. Through internships and projects, I’ve worked on models, dashboards, and making strategic recommendations. I bring a mix of analytical skills and business insight, making me well-suited to bridge data and strategy. I’m currently pursuing opportunities in business analytics, finance, and strategy/operations.",
   skills: ["Python", "SQL", "R", "Microsoft Excel", "Tableau", "SAP"],
+
   projects: [
     {
       name: "Sales KPI Dashboard",
@@ -27,37 +28,39 @@ export const siteConfig = {
       skills: ["Python", "Machine Learning"],
     },
   ],
-      experience: [
-        {
-          company: "Vetri Labs",
-          title: "Business Analyst Intern",
-          dateRange: "Feb 2024 - Jan 2025",
-          bullets: [
-            "Built a financial model in Excel projecting 1-2 year growth, used by the CEO and leadership team to assess funding strategies and refine business goals.",
-            "Analyzed regional competitors’ market share, pricing, and customer segments to highlight differentiation opportunities and define the company’s unique value proposition.",
-            "Initiated and led the development of the company’s SBA Growth Accelerator Fund application by compiling financial data and crafting strategic proposals that strengthened the company’s case for federal funding.",
-          ],
-        },
-        {
-          company: "Fernweh Group",
-          title: "Finance Intern",
-          dateRange: "Sep 2023 - Jan 2024",
-          bullets: [
-            "Managed and cleaned 10,000+ rows of employee expense data in Excel to support monthly operating cost reports.",
-            "Reconciled internal records with financial statements to ensure reporting accuracy for the VP of Finance.",
-          ],
-        },
+
+  experience: [
+    {
+      company: "Vetri Labs",
+      title: "Business Analyst Intern",
+      dateRange: "Feb 2024 - Jan 2025",
+      bullets: [
+        "Built a financial model in Excel projecting 1-2 year growth, used by the CEO and leadership team to assess funding strategies and refine business goals.",
+        "Analyzed regional competitors’ market share, pricing, and customer segments to highlight differentiation opportunities and define the company’s unique value proposition.",
+        "Initiated and led the development of the company’s SBA Growth Accelerator Fund application by compiling financial data and crafting strategic proposals that strengthened the company’s case for federal funding.",
       ],
-      education: [
-        {
-          school: "San Jose State University",
-          degree: "Bachelor of Science in Economics",
-          dateRange: "2023 - 2025",
-        },
-        {
-          school: "University of California San Diego Extended Studied",
-          degree: "Statistics for Data Analytics",
-          dateRange: "2025",
+    },
+    {
+      company: "Fernweh Group",
+      title: "Finance Intern",
+      dateRange: "Sep 2023 - Jan 2024",
+      bullets: [
+        "Managed and cleaned 10,000+ rows of employee expense data in Excel to support monthly operating cost reports.",
+        "Reconciled internal records with financial statements to ensure reporting accuracy for the VP of Finance.",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      school: "San Jose State University",
+      degree: "Bachelor of Science in Economics",
+      dateRange: "2023 - 2025",
+    },
+    {
+      school: "University of California San Diego Extended Studies",
+      degree: "Statistics for Data Analytics",
+      dateRange: "2025",
     },
   ],
 };
